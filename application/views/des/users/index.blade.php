1cf5d7d16ae52e5a0c79247280e0bea3ca8ccc0b
Chalormsak S. <s.chalormsak@gmail.com>
2025-09-29 17:39:11 +0700

    feat: Enhance designer user management; add group selection and update functionality in user table

--- application/views/des/users/index.blade.php
+++ application/views/des/users/index.blade.php
@@ -3,10 +3,44 @@
 @section('contents')
     <h2 class="card-title text-2xl">Designer Users</h2>
     <div class="divider m-0"></div>
-    <table id="table" class="table table-zebra display text-xs"></table>
+    <table id="table" class="table table-zebra display text-xs">
+        <thead>
+            <tr>
+                <th rowspan="2">User ID</th>
+                <th rowspan="2">Department</th>
+                <th rowspan="2">Section</th>
+                <th colspan="4">Group</th>
+                <th rowspan="2">Designer</th>
+                <th rowspan="2">Checker</th>
+            </tr>
+            <tr>
+                <th>EME</th>
+                <th>EEL</th>
+                <th>EAP</th>
+                <th>ESO</th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
 @endsection
-
 
 @section('scripts')
     <script src="{{ $_ENV['APP_JS'] }}/des_users.js?ver={{ $GLOBALS['version'] }}"></script>
+@endsection
+
+@section('styles')
+    <style>
+        .filterBtnDt {
+            align-items: center !important;
+            margin-left: 10px;
+        }
+
+        .filter-btn-dt:not(.btn-primary) {
+            border: 1px solid var(--color-gray-500);
+        }
+
+        .filter-btn-dt.btn-primary {
+            color: white;
+        }
+    </style>
 @endsection
