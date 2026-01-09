e70a8b77dbc1335bde31bebf8756d6b024ea3650
Chalormsak S. <s.chalormsak@gmail.com>
2026-01-09 17:15:40 +0700

    refactor orders and excel scripts to enhance data handling and export functionality

--- assets/script/mar/orders.js
+++ assets/script/mar/orders.js
@@ -2,14 +2,17 @@ import "@amec/webasset/css/dataTable.min.css";
 import dayjs from "dayjs";
 import ExcelJS from "exceljs";
 import { createTable } from "@amec/webasset/dataTable";
-import { getTemplate, exportExcel } from "../service/excel";
+import { getTemplate, exportExcel, copyMergedRow } from "../service/excel";
 import * as utils from "../utils.js";
 
 var table;
 $(async function () {
 	try {
 		await utils.initApp();
-		const data = await loadTableData({ ORDER_NO: "PT-4362-0-D" });
+		const data = await loadTableData({
+			SINQ_DATE: dayjs().subtract(3, "month").format("YYYY-MM-DD"),
+			IS_ORDERS: 1,
+		});
 		const opt = await tableOrdersOption(data);
 		table = await createTable(opt);
 	} catch (error) {
@@ -21,6 +24,18 @@ $(async function () {
 });
 
 async function tableOrdersOption(data) {
+	const uniqueData = data.reduce((acc, current) => {
+		const exists = acc.find(
+			(item) =>
+				item.INQUIRY_NO === current.INQUIRY_NO &&
+				item.PRJ_NO === current.PRJ_NO
+		);
+		if (!exists) {
+			acc.push(current);
+		}
+		return acc;
+	}, []);
+	data = uniqueData;
 	const opt = { ...utils.tableOpt };
 	opt.data = data;
 	opt.columns = [
@@ -33,7 +48,7 @@ async function tableOrdersOption(data) {
 			},
 		},
 		{
-			data: "INQUIRY_NO",
+			data: "INQ_NO",
 			title: "Inquiry No.",
 			className: "text-nowrap",
 		},
@@ -76,7 +91,7 @@ async function tableOrdersOption(data) {
 			className: "text-nowrap",
 		},
 		{
-			data: "INQUIRY_NO",
+			data: "INQ_NO",
 			className: "w-fit !max-w-[110px] !justify-center",
 			sortable: false,
 			title: `<div class="flex justify-center"><i class="fi fi-rr-settings-sliders text-lg"></i></div>`,
@@ -122,8 +137,10 @@ async function loadTableData(q) {
 $(document).on("click", ".export-docs", async function (e) {
 	e.preventDefault();
 	try {
+		const docs = table.row($(this).closest("tr")).data().PRJ_NO;
+		const data = await loadTableData({ PRJ_NO: docs, IS_ORDERS: 1 });
 		const template = await getTemplate("cover_sheet_orders.xlsx");
-		await exportDocument(template);
+		await exportDocument(template, data);
 	} catch (error) {
 		console.log(error);
 		await utils.errorMessage(error);
@@ -144,12 +161,12 @@ $(document).on("click", "#export-btn", async function (e) {
 	}
 });
 
-async function exportDocument(template) {
+async function exportDocument(template, data) {
 	const file = template.buffer;
 	const workbook = new ExcelJS.Workbook();
 	await workbook.xlsx.load(file).then(async (workbook) => {
 		//Process sheets
-		await sheet1(workbook);
+		await sheet1(workbook, data);
 		await sheet2(workbook);
 		await sheet3(workbook);
 		await sheet4(workbook);
@@ -168,9 +185,59 @@ async function exportDocument(template) {
 }
 
 //Sheet 1: Cover Page
-async function sheet1(workbook) {
+async function sheet1(workbook, data) {
 	const sheet = workbook.worksheets[0];
-	sheet.getCell(1, 1).value = "AAA";
+	sheet.getCell("D2").value = dayjs(data[0].IDS_DATE).format("YYYY-MM-DD");
+	sheet.getCell("D3").value = data[0].CREATEBY;
+	sheet.getCell("D9").value = data[0].TRADER;
+	sheet.getCell("D10").value = data[0].PRJ_NO;
+	sheet.getCell("D11").value = data[0].PRJ_NAME;
+	sheet.getCell("D12").value = data[0].MFGNO;
+	sheet.getCell("D13").value = data[0].INQ_NO;
+
+	sheet.getCell("S9").value = data[0].AGENT;
+	sheet.getCell("S10").value = data[0].DSTN;
+	sheet.getCell("S11").value = data[0].AMEC_SCHDL;
+	sheet.getCell("S12").value = dayjs(data[0].CUST_RQS).format("YYYY-MM-DD");
+	sheet.getCell("S13").value = data[0].SHIP;
+	sheet.getCell("Z11").value = data[0].PT;
+
+	const rowNum = 17;
+	const mergeDetails = sheet._merges; // เข้าถึงข้อมูลการ Merge ทั้งหมดใน Sheet
+	const foundMerge = Object.values(mergeDetails).find(
+		(m) => m.top <= rowNum && m.bottom >= rowNum && m.left === 1 // left 1 คือ Column A
+	);
+	if (foundMerge) {
+		console.log(`พบการ Merge ที่: ${foundMerge.shortAddr}`);
+		// จะได้ค่าอย่างเช่น "A17:AA17"
+	}
+	// let i = 17;
+	// let j = 1;
+	// data.forEach((val) => {
+	// 	if (j >= 14) {
+	// 		cloneRows(sheet, 18, i);
+	// 	}
+	// 	sheet.getCell(i, 1).value = j;
+	// 	sheet.getCell(i, 2).value = val.CAR_NO;
+	// 	sheet.getCell(i, 3).value = val.INQD_ITEM;
+	// 	sheet.getCell(i, 5).value = val.INQD_PARTNAME;
+	// 	sheet.getCell(i, 8).value = val.INQD_DRAWING;
+	// 	sheet.getCell(i, 11).value = val.INQD_VARIABLE;
+	// 	sheet.getCell(i, 14).value = val.CSQTY;
+	// 	sheet.getCell(i, 15).value = val.INQD_UNIT;
+	// 	sheet.getCell(i, 16).value = "";
+	// 	sheet.getCell(i, 18).value = val.MFGNO;
+	// 	sheet.getCell(i, 21).value = getSchedule(val.MARREQPRDN);
+	// 	sheet.getCell(i, 23).value = getSchedule(val.AMEC_SCHDL);
+	// 	sheet.getCell(i, 25).value = val.REMARK;
+	// 	i++;
+	// 	j++;
+	// });
+
+	// let page = 0;
+	// if (j > 14) {
+	// 	page = Math.ceil((j - 14) / 28);
+	// }
 }
 
 //Sheet 2: Order list
@@ -194,4 +261,32 @@ async function sheet4(workbook) {
 	sheet.getCell(1, 1).value = "AAA";
 	sheet.getCell(1, 1).value = "AAA";
 	sheet.getCell(1, 1).value = "AAA";
+}
+
+function getSchedule(val) {
+	if (val == "" || val == undefined || val == null) return "";
+
+	const str1 = dayjs(val).format("YYMM");
+	let jun = "";
+	switch (dayjs(val).format("DD")) {
+		case "05":
+			jun = `${str1}X`;
+			break;
+		case "10":
+			jun = `${str1}A`;
+			break;
+		case "15":
+			jun = `${str1}Y`;
+			break;
+		case "20":
+			jun = `${str1}B`;
+			break;
+		case "25":
+			jun = `${str1}Z`;
+			break;
+		default:
+			jun = `${str1}C`;
+			break;
+	}
+	return jun;
 }
--- assets/script/service/excel.js
+++ assets/script/service/excel.js
@@ -5,167 +5,192 @@ import dayjs from "dayjs";
 var daterange;
 
 export const cloneRows = async (worksheet, sourceRowNum, targetRowNum) => {
-  const sourceRow = worksheet.getRow(sourceRowNum);
-  const newRow = worksheet.insertRow(targetRowNum);
-  sourceRow.eachCell({ includeEmpty: true }, (cell, colNumber) => {
-    const newCell = newRow.getCell(colNumber);
-    if (cell.style) {
-      newCell.style = { ...cell.style };
-    }
-  });
-  newRow.height = sourceRow.height;
+	const sourceRow = worksheet.getRow(sourceRowNum);
+	const newRow = worksheet.insertRow(targetRowNum);
+	sourceRow.eachCell({ includeEmpty: true }, (cell, colNumber) => {
+		const newCell = newRow.getCell(colNumber);
+		if (cell.style) {
+			newCell.style = { ...cell.style };
+			//newCell.style = JSON.parse(JSON.stringify(cell.style));
+		}
+	});
+	newRow.height = sourceRow.height;
+};
+
+export const copyMergedRow = async (worksheet, sourceRange, targetRange) => {
+	// 1. ดึงข้อมูลจาก Cell แรกของ Source (Master Cell)
+	const sourceMasterCell = worksheet.getCell(sourceRange.split(":")[0]); // ดึง A17
+	const targetMasterCell = worksheet.getCell(targetRange.split(":")[0]); // ดึง A25
+
+	// 2. สั่ง Merge ที่ตำแหน่งใหม่
+	worksheet.mergeCells(targetRange);
+
+	// 3. Copy Value และ Style จาก Master ไปยัง Master ใหม่
+	targetMasterCell.value = sourceMasterCell.value;
+
+	if (sourceMasterCell.style) {
+		targetMasterCell.style = JSON.parse(
+			JSON.stringify(sourceMasterCell.style)
+		);
+	}
+
+	// 4. (Optional) ปรับความสูงของแถวให้เท่ากัน
+	const sourceRow = worksheet.getRow(sourceMasterCell.row);
+	const targetRow = worksheet.getRow(targetMasterCell.row);
+	targetRow.height = sourceRow.height;
 };
 
 export const exportExcel = async (data, template, options = {}) => {
-  const opt = {
-    filename: `export.xlsx`,
-    rowstart: 2,
-    ...options,
-  };
-  daterange = await getCalendar(data);
-  const file = template.buffer;
-  const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.load(file).then(async (workbook) => {
-    const sheet_data = workbook.worksheets[1];
-    const columns = await exportFormat(sheet_data);
-    const sheet = workbook.worksheets[0];
-    const colCount = sheet.columnCount;
-    const white = opt.rowstart;
-    const gray = opt.rowstart + 1;
-    let color = white;
+	const opt = {
+		filename: `export.xlsx`,
+		rowstart: 2,
+		...options,
+	};
+	daterange = await getCalendar(data);
+	const file = template.buffer;
+	const workbook = new ExcelJS.Workbook();
+	await workbook.xlsx.load(file).then(async (workbook) => {
+		const sheet_data = workbook.worksheets[1];
+		const columns = await exportFormat(sheet_data);
+		const sheet = workbook.worksheets[0];
+		const colCount = sheet.columnCount;
+		const white = opt.rowstart;
+		const gray = opt.rowstart + 1;
+		let color = white;
 
-    let target = opt.rowstart;
-    data.forEach(async (el, i) => {
-      target = i + opt.rowstart;
-      if (target > opt.rowstart + 1) {
-        cloneRows(sheet, color, target);
-        color = i % 2 == 0 ? gray : white;
-      }
+		let target = opt.rowstart;
+		data.forEach(async (el, i) => {
+			target = i + opt.rowstart;
+			if (target > opt.rowstart + 1) {
+				cloneRows(sheet, color, target);
+				color = i % 2 == 0 ? gray : white;
+			}
 
-      for (let j = 1; j <= colCount; j++) {
-        const format = columns.find((item) => item[1] == j);
-        if (format == undefined) continue;
-        let value = "";
-        if (format[3] == null) continue;
-        if (format[2] == "Func") {
-          const param = format[4] ? JSON.parse(format[4]) : {};
-          value = eval(format[3])(el, param);
-        } else if (format[2] == "Formula") {
-          value = { formula: format[3].replaceAll("{x}", target) };
-        } else {
-          if (format[3].includes(".")) {
-            value = el;
-            const keys = format[3].split(".");
-            for (const key of keys) {
-              value = value[key] !== undefined ? value[key] : "";
-            }
-          } else {
-            value = el[format[3]] !== undefined ? el[format[3]] : "";
-          }
-        }
+			for (let j = 1; j <= colCount; j++) {
+				const format = columns.find((item) => item[1] == j);
+				if (format == undefined) continue;
+				let value = "";
+				if (format[3] == null) continue;
+				if (format[2] == "Func") {
+					const param = format[4] ? JSON.parse(format[4]) : {};
+					value = eval(format[3])(el, param);
+				} else if (format[2] == "Formula") {
+					value = { formula: format[3].replaceAll("{x}", target) };
+				} else {
+					if (format[3].includes(".")) {
+						value = el;
+						const keys = format[3].split(".");
+						for (const key of keys) {
+							value = value[key] !== undefined ? value[key] : "";
+						}
+					} else {
+						value =
+							el[format[3]] !== undefined ? el[format[3]] : "";
+					}
+				}
 
-        if (format[2] === "Date" && value) {
-          sheet.getCell(target, format[1]).value =
-            dayjs(value).format("YYYY-MM-DD");
-        } else if (format[2] === "Datetime" && value) {
-          sheet.getCell(target, format[1]).value = dayjs(value).format(
-            "YYYY-MM-DD HH:mm:ss"
-          );
-        } else {
-          sheet.getCell(target, format[1]).value = value;
-        }
-      }
-    });
+				if (format[2] === "Date" && value) {
+					sheet.getCell(target, format[1]).value =
+						dayjs(value).format("YYYY-MM-DD");
+				} else if (format[2] === "Datetime" && value) {
+					sheet.getCell(target, format[1]).value = dayjs(
+						value
+					).format("YYYY-MM-DD HH:mm:ss");
+				} else {
+					sheet.getCell(target, format[1]).value = value;
+				}
+			}
+		});
 
-    if (opt.static) {
-      for (let rw of opt.static) {
-        sheet.getCell(rw.cols).value = rw.text;
-      }
-    }
+		if (opt.static) {
+			for (let rw of opt.static) {
+				sheet.getCell(rw.cols).value = rw.text;
+			}
+		}
 
-    // Excute more options (if any)
-    if (opt.execute != null && typeof opt.execute == "function") {
-      await opt.execute(workbook, sheet);
-    }
+		// Excute more options (if any)
+		if (opt.execute != null && typeof opt.execute == "function") {
+			await opt.execute(workbook, sheet);
+		}
 
-    // Remove all sheets except the first one
-    while (workbook.worksheets.length > 1) {
-      workbook.removeWorksheet(workbook.worksheets[1].id);
-    }
-    await workbook.xlsx.writeBuffer().then(function (buffer) {
-      const blob = new Blob([buffer], {
-        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-      });
-      const link = document.createElement("a");
-      link.href = URL.createObjectURL(blob);
-      link.download = opt.filename;
-      link.click();
-    });
-  });
+		// Remove all sheets except the first one
+		while (workbook.worksheets.length > 1) {
+			workbook.removeWorksheet(workbook.worksheets[1].id);
+		}
+		await workbook.xlsx.writeBuffer().then(function (buffer) {
+			const blob = new Blob([buffer], {
+				type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+			});
+			const link = document.createElement("a");
+			link.href = URL.createObjectURL(blob);
+			link.download = opt.filename;
+			link.click();
+		});
+	});
 };
 
 async function getCalendar(data) {
-  const minInqMoment = data.reduce((acc, item) => {
-    if (!item || !item.INQ_DATE) return acc;
-    const m = dayjs(item.INQ_DATE);
-    if (!m.isValid()) return acc;
-    return acc === null || m.isBefore(acc) ? m : acc;
-  }, null);
+	const minInqMoment = data.reduce((acc, item) => {
+		if (!item || !item.INQ_DATE) return acc;
+		const m = dayjs(item.INQ_DATE);
+		if (!m.isValid()) return acc;
+		return acc === null || m.isBefore(acc) ? m : acc;
+	}, null);
 
-  const minInqDate = minInqMoment ? minInqMoment.format("YYYYMMDD") : null;
-  const daterange = await ameccaledar(
-    minInqDate,
-    dayjs().add(10, "days").format("YYYYMMDD")
-  );
-  return daterange;
+	const minInqDate = minInqMoment ? minInqMoment.format("YYYYMMDD") : null;
+	const daterange = await ameccaledar(
+		minInqDate,
+		dayjs().add(10, "days").format("YYYYMMDD")
+	);
+	return daterange;
 }
 
 function getEffect(data, param) {
-  const inqgroup = data.inqgroup;
-  const des = inqgroup.filter((item) => item.INQG_GROUP === param.INQG_GROUP);
-  return des.length > 0 ? "Y" : "";
+	const inqgroup = data.inqgroup;
+	const des = inqgroup.filter((item) => item.INQG_GROUP === param.INQG_GROUP);
+	return des.length > 0 ? "Y" : "";
 }
 
 function nextWorkingDay(data, param) {
-  const sdate = dayjs(data.timeline.MAR_SEND).format("YYYYMMDD");
-  const days = parseInt(param.days);
-  daterange = daterange.filter(
-    (item) => item.DAYOFF == 0 && item.WORKID >= sdate
-  );
-  let i = 1;
-  let current = sdate;
-  daterange.forEach((item) => {
-    if (i == days) {
-      current = item.WORKID;
-    }
-    i++;
-  });
-  return dayjs(current, "YYYYMMDD").format("YYYY-MM-DD");
+	const sdate = dayjs(data.timeline.MAR_SEND).format("YYYYMMDD");
+	const days = parseInt(param.days);
+	daterange = daterange.filter(
+		(item) => item.DAYOFF == 0 && item.WORKID >= sdate
+	);
+	let i = 1;
+	let current = sdate;
+	daterange.forEach((item) => {
+		if (i == days) {
+			current = item.WORKID;
+		}
+		i++;
+	});
+	return dayjs(current, "YYYYMMDD").format("YYYY-MM-DD");
 }
 
 export const getTemplate = async (filename) => {
-  const data = {
-    path: `${process.env.FILE_TEMPLATE}/${filename}`,
-    name: filename,
-  };
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      url: `${process.env.APP_API}/files/template/read/`,
-      type: "POST",
-      dataType: "json",
-      data: data,
-      success: function (res) {
-        const binaryData = atob(res.content);
-        const buffer = new Uint8Array(binaryData.length);
-        for (let i = 0; i < binaryData.length; i++) {
-          buffer[i] = binaryData.charCodeAt(i);
-        }
-        res.buffer = buffer;
-        resolve(res);
-      },
-      error: function (error) {
-        reject(error);
-      },
-    });
-  });
+	const data = {
+		path: `${process.env.FILE_TEMPLATE}/${filename}`,
+		name: filename,
+	};
+	return new Promise((resolve, reject) => {
+		$.ajax({
+			url: `${process.env.APP_API}/files/template/read/`,
+			type: "POST",
+			dataType: "json",
+			data: data,
+			success: function (res) {
+				const binaryData = atob(res.content);
+				const buffer = new Uint8Array(binaryData.length);
+				for (let i = 0; i < binaryData.length; i++) {
+					buffer[i] = binaryData.charCodeAt(i);
+				}
+				res.buffer = buffer;
+				resolve(res);
+			},
+			error: function (error) {
+				reject(error);
+			},
+		});
+	});
 };
